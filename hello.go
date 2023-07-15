62e01c978f7ea0105eadb946aa8182bf2aef8705
DatTran <khiemtran21012000@gmail.com>
2023-07-15 16:02:50 +0700

    add code for channel

--- hello.go
+++ hello.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Print("Hello Guys !!!")
+	var wg = sync.WaitGroup{}
+	ch := make(chan int, 50) // create a channel with 50 buffers
+
+	wg.Add(2)
+	go func(ch <-chan int) {
+		// i := <-ch // get a value from channel. Waiting for value assigned in channel
+		for i := range ch {
+			fmt.Println(i)
+		}
+		wg.Done()
+	}(ch)
+	go func(ch chan<- int) {
+		ch <- 42 // Set value to ch channel
+		ch <- 50
+		ch <- 50
+		ch <- 50
+		close(ch) // close channel
+		wg.Done()
+	}(ch)
+	wg.Wait()
 }
