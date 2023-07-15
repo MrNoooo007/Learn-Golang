f99964358dad8fbadcb58b2f0a7bfc2a1d29efc7
DatTran <khiemtran21012000@gmail.com>
2023-07-15 15:26:00 +0700

    add code for mutex

--- hello.go
+++ hello.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var counter = 0
+var wg = sync.WaitGroup{}
+var m = sync.RWMutex{}
 
 func main() {
-	fmt.Print("Hello Guys !!!")
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock() // to read counter
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Println("Hello ", counter)
+	m.RUnlock() // unlock counter
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
 }
