8c81a72d78a7d6195557ccd588c7f6aca8743aed
DatTran <khiemtran21012000@gmail.com>
2023-07-15 20:53:35 +0700

    add code gin framework

--- hello.go
+++ hello.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	"net/http"
+
+	gin "github.com/gin-gonic/gin"
 )
 
 func main() {
-	var wg = sync.WaitGroup{}
-	ch1 := make(chan int, 50) // create a channel with 50 buffers
-	ch2 := make(chan int, 50) // create a channel with 50 buffers
-
-	wg.Add(2)
-	go func() {
-		for {
-			select {
-			case i := <-ch1:
-				fmt.Print("Channel 1 ", i)
-			case j := <-ch2:
-				fmt.Print("channel 2 ", j)
-			default:
-				break
-			}
-		}
-		wg.Done()
-	}()
-	go func() {
-		ch1 <- 42  // Set value to ch channel
-		close(ch1) // close channel
-
-		ch2 <- 50
-		close(ch2)
-		wg.Done()
-	}()
-	wg.Wait()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
